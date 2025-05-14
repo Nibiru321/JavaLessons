27a501c7a1b7b4a2c550cdf97b4941af63d99c5f
Nibiru321 <umeshmuralitharan937@gmail.com>
2025-05-14 15:24:04 +0800

    prime

--- main.java
+++ main.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
-class Main {
+
+class main {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int number;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        char choice;
-        do {
-            System.out.print("Enter the number ");
-            number = scn.nextInt();
-
-            if(number > max){
-                max = number;
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        for(int n = low; n <= high; n++){
+            int count = 0;
+            for(int div = 2; div * div <= n; div++){
+                if(n%div == 0){
+                    count++;
+                    break;
+                }
             }
-
-            if(number < min){
-                min = number;
+            if(count == 0){
+                System.out.println(n);
             }
-            System.out.print("Do you want to continue y/n? ");
-            choice = scn.next().charAt(0);
-
-        }while(choice == 'y' || choice == 'Y');
-
-        System.out.println("Largest number: " + max);
-        System.out.println("Smallest number: " + min);
-    }
+        }
+        
+    }    
 }
