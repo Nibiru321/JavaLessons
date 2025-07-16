460b3ec5ec4fb84223df3390769569df095548f3
Nibiru321 <umeshmuralitharan937@gmail.com>
2025-07-16 14:36:33 +0800

    lSum

---
+++ LargestSum.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LargestSum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scn.nextInt();
+
+        int[] a = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            a[i] = scn.nextInt();
+        }
+
+        System.out.println("Maximum contiguous sum is " + maxSubArraySum(a));
+    }
+
+    static int maxSubArraySum(int a[]) {
+        int size = a.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+        for (int i = 0; i < size; i++) {
+            max_ending_here = max_ending_here + a[i];
+
+            if (max_so_far < max_ending_here) {
+                max_so_far = max_ending_here;
+            }
+
+            if (max_ending_here < 0) {
+                max_ending_here = 0;
+            }
+        }
+        return max_so_far;
+    }
+}
